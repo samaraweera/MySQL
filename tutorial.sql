f50f0ded7acd92261ac4ac1862be19800302bad1
Matrix <kdpsamaraweera@gmail.com>
2019-12-31 16:58:46 +0530

    Initial commit

---
+++ tutorial.sql
@@ -0,0 +1,11 @@
+This project belongs to a combination of Nokia and Cell Card in Cambodia.Cellcard Cambolia is a telecommunication service provider in Cambodia.The requirement was to implement WSO2 ESB within their CCBS infrastructure. Backend service elements such as Billing (6D), HLR, Surepay (Nokia) This project is developed as the middle layer mainly it is included APIs, Proxy's which are implemented using WSO2 ESB.
+
+Cellcard Cambolia is a telecommunication service provider in Cambodia. The requirement was to implement WSO2 ESB within their CCBS infrastructure. Backend service elements such as Billing (6D), HLR, Surepay (Nokia) were integrated with the ESB to convert REST to SOAP and SOAP to REST as needed.
+
+Environment / Tools:
+WSO2 ESB 5.0, Web Services, MS Project, SVN
+(WSO2 Enterprise Service Bus(ESB), Java, XML, Rest and SOAP web services)
+
+
+
+MAgri system of MVP module consists of both web applications and mobile applications. This system used to manage the information flows between value chain parties to enhance farmer relations in the agriculture business.
